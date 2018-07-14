748d5a502f31fece2d082a34690ef0de26f3b72f
dantleech <dan.t.leech@gmail.com>
2018-07-14 16:18:26 +0200

    Made create a single argument method

--- tests/Unit/Bridge/TolerantParser/ChainTolerantCompletorTest.php
+++ tests/Unit/Bridge/TolerantParser/ChainTolerantCompletorTest.php
@@ -39,7 +39,7 @@ class ChainTolerantCompletorTest extends TestCase
         )->willReturn(
             Response::fromSuggestions(
                 Suggestions::fromSuggestions([
-                    Suggestion::create('v', 'foo', 'bar')
+                    Suggestion::create('foo')
                 ])
             )
         );
